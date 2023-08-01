768be6c9095faf52a79ecf0f938c3cb404c6a16d
Rishita Singh <rishitasingh358@gmail.com>
2023-08-01 09:44:31 +0530

    adding second file

---
+++ demo.js
@@ -0,0 +1,5 @@
+// console.dir(document);
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+
+header.style.borderBottom = 'solid 3px #000';
