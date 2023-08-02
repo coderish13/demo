2c13b8f9309549fb2822401daff381b9a07a7fb2
Rishita Singh <rishitasingh358@gmail.com>
2023-08-02 08:46:36 +0530

    fourth changes

--- demo.js
+++ demo.js
@@ -8,8 +8,18 @@ var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 // console.log(items[1]);
 items[1].style.fontWeight = 'bold';
-items[2].style.backgroundColor = 'green';
+// items[2].style.backgroundColor = 'green';
 
 for(var i=0; i<items.length; i++) {
     items[i].style.fontWeight = 'bold';
 }
+
+var item = document.querySelector('list-group-item');
+// items[1].style.backgroundColor = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0; i<odd.length; i++) {
+    odd[i].style.backgroundColor = 'green';
+}
+
